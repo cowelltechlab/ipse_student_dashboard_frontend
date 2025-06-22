0829c158485776c7ed34e93a716fcfe88579e6d2
alejandra-e-bernabel <alejandra.e.bernabel@gmail.com>
2025-06-21 21:58:35 -0400

    Added spinner and styling to callback page

--- src/pages/Callback.tsx
+++ src/pages/Callback.tsx
@@ -2,6 +2,31 @@ import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import useAuth from "../contexts/useAuth";
 
+const spinnerStyle: React.CSSProperties = {
+  width: "48px",
+  height: "48px",
+  border: "6px solid #ccc",
+  borderTop: "6px solid #1976d2",
+  borderRadius: "50%",
+  animation: "spin 1s linear infinite",
+  marginBottom: "20px",
+};
+
+const containerStyle: React.CSSProperties = {
+  minHeight: "100vh",
+  background: "#fff",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+};
+
+const spinnerKeyframes = `
+@keyframes spin {
+  to { transform: rotate(360deg); }
+}
+`;
+
 const OAuthCallbackHandler = () => {
   const { handleCallback } = useAuth();
   const navigate = useNavigate();
@@ -18,7 +43,13 @@ const OAuthCallbackHandler = () => {
     }
   }, [handleCallback, navigate]);
 
-  return <p>Authenticating with Google...</p>;
+  return (
+    <div style={containerStyle}>
+      <style>{spinnerKeyframes}</style>
+      <div style={spinnerStyle}></div>
+      <p>Authenticating with Google...</p>
+    </div>
+  );
 };
 
 export default OAuthCallbackHandler;
