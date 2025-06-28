fa35f9fca7b3115a1cb18401f4bbf5d7f5ef133f
Rani Hinnawi <rhinnawi95@gmail.com>
2025-06-28 13:43:32 -0700

    added page header

--- src/pages/CreateNewAssignment.tsx
+++ src/pages/CreateNewAssignment.tsx
@@ -1,13 +1,20 @@
 import { Box } from "@chakra-ui/react";
+import CreateNewAssignmentIcon from "../assets/Create New Assignment.svg"
 import PageHeader from "../components/common/pageHeader/PageHeader";
-import HeaderCard from "../components/homeDashboard/HomeHeaderCard";
+import HeaderCard from "../components/common/pageHeader/HeaderCard";
 import HomeContent from "../components/homeDashboard/HomeContent";
 
 const CreateNewAssignment = () => {
+  const cardText = `Create an assignment that adapts and grows to support every
+                     student's needs. Let's make learning accessible for all!`;
   return (
     <Box>
       <PageHeader />
-      <HeaderCard />
+      <HeaderCard 
+        cardHeading="Create Assignment" 
+        cardText={cardText} 
+        cardImageUrl={CreateNewAssignmentIcon}
+      />
       <HomeContent />
     </Box>
   );
