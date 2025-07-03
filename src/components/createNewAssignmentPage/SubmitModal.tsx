ac48f0bc1bfdcaa450eb924658c1e79d2cbdc55c
Rani Hinnawi <rhinnawi95@gmail.com>
2025-07-03 15:25:03 -0700

    added placeholder for submitmodal button

---
+++ src/components/createNewAssignmentPage/SubmitModal.tsx
@@ -0,0 +1,15 @@
+import {
+    Box,
+    Text
+} from "@chakra-ui/react";
+
+// TODO: Implement 
+const SubmitModal = () => {
+    return (
+        <Box>
+            <Text>Modal</Text>
+        </Box>
+    )
+}
+
+export default SubmitModal;
