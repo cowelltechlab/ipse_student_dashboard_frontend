8f70271a857583c2ed79c9468c9290bbb57e2cef
Alejandra Bernabel <alejandra.e.bernabel@gmail.com>
2025-10-12 00:39:19 -0400

    Fixed import

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { Provider } from "./components/ui/provider";
-import { AuthProvider } from "./contexts/AuthContext";
+import { AuthProvider } from "./contexts/authContext";
 
 import "@fontsource-variable/comme/index.css";
 
